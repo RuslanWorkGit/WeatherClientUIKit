f2acbc231f16dc2507d1906c24c537d642935517
RuslanWorkGit <lulka758@gmail.com>
2025-01-27 19:51:40 +0200

    make main screen UI
    add segment controller for chose city or coordinates
    add UITextField for city, lattitude, longitude
    add saerch button

--- WeatherClientUIKit/WeatherClientUIKit/ViewController.swift
+++ WeatherClientUIKit/WeatherClientUIKit/ViewController.swift
@@ -8,11 +8,165 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let cityTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "city Name"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let latitudeTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "latitude"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let longitudeUITextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "longitude"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let searchButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Search 🔎", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let segmentedControl: UISegmentedControl = {
+        let segmentController = UISegmentedControl(items: ["City", "Coordinates"])
+        segmentController.translatesAutoresizingMaskIntoConstraints = false
+        return segmentController
+    }()
+    
+    private var cityConstraint: [NSLayoutConstraint] = []
+    private var coordinateConstraint: [NSLayoutConstraint] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupAction()
     }
+    
+    func setupUI() {
+        
+        view.addSubview(cityTextField)
+        view.addSubview(latitudeTextField)
+        view.addSubview(longitudeUITextField)
+        view.addSubview(searchButton)
+        view.addSubview(segmentedControl)
+        
+        segmentedControl.selectedSegmentIndex = 0
+        
+        
+        NSLayoutConstraint.activate([
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+        ])
+        
+        cityConstraint = [
+            cityTextField.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20),
+            cityTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            cityTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            searchButton.topAnchor.constraint(equalTo: cityTextField.bottomAnchor, constant: 20),
+            searchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchButton.heightAnchor.constraint(equalToConstant: 50),
+            searchButton.widthAnchor.constraint(equalToConstant: 150)
+        ]
+        
+        coordinateConstraint = [
+            longitudeUITextField.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20),
+            longitudeUITextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            longitudeUITextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            latitudeTextField.topAnchor.constraint(equalTo: longitudeUITextField.bottomAnchor, constant: 20),
+            latitudeTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            latitudeTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            searchButton.topAnchor.constraint(equalTo: latitudeTextField.bottomAnchor, constant: 20),
+            searchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchButton.heightAnchor.constraint(equalToConstant: 50),
+            searchButton.widthAnchor.constraint(equalToConstant: 150)
+        ]
+        
+        NSLayoutConstraint.activate(cityConstraint)
+        
+        //toggleFields(for: segmentedControl.selectedSegmentIndex)
+
+    }
+    
+    private func setupAction() {
+        searchButton.addTarget(self, action: #selector(searchButtonAction), for: .touchUpInside)
+        segmentedControl.addTarget(self, action: #selector(segmentControValueChange), for: .valueChanged)
+    }
+    
+//    private func toggleFields(for index: Int) {
+//        cityTextField.isHidden = index != 0
+//        longitudeUITextField.isHidden = index == 0
+//        latitudeTextField.isHidden = index == 0
+//    }
+    
+    @objc func segmentControValueChange(_ sender: UISegmentedControl){
+//        toggleFields(for: sender.selectedSegmentIndex)
+        
+        if segmentedControl.selectedSegmentIndex == 0 {
+            cityTextField.isHidden = false
+            longitudeUITextField.isHidden = true
+            latitudeTextField.isHidden = true
+            
+            NSLayoutConstraint.deactivate(coordinateConstraint)
+            NSLayoutConstraint.activate(cityConstraint)
+        } else {
+            cityTextField.isHidden = true
+            longitudeUITextField.isHidden = false
+            latitudeTextField.isHidden = false
+            
+            NSLayoutConstraint.deactivate(cityConstraint)
+            NSLayoutConstraint.activate(coordinateConstraint)
+            
+        }
+    }
+    
+    @objc func searchButtonAction() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            guard let city = cityTextField.text, !city.isEmpty else {
+                assertionFailure("Please enter city name")
+                return
+            }
+        } else {
+            guard let latitude = latitudeTextField.text, !latitude.isEmpty else {
+                assertionFailure("Please enter latitude")
+                return
+            }
+            
+            guard let longitude = longitudeUITextField.text, !longitude.isEmpty else {
+                assertionFailure("Please enter longitude")
+                return
+            }
+            
+
+        }
+    }
+    
+    private func fetchWeather(byCyti city: String) {
+        
+        print("Fetching weather for city: \(city)")
+    }
+    
+    private func fetchWeather(byLatitude latitude: String, byLongitude longitude: String) {
+        
+        print("Fetching weather for latitude: \(latitude), longitude: \(longitude)")
+    }
+
 
 
 }
